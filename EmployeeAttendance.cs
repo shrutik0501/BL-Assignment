7d925f7f0450af3927c15450419c43b027472224
shrutik0501 <shrutik0501@gmail.com>
2023-03-26 16:53:57 +0530

    my UC4

--- EmployeeAttendance.cs
+++ EmployeeAttendance.cs
@@ -13,20 +13,35 @@ namespace EmployeeManagement
             int IS_PART_TIME  = 1;
             int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
+            int NUM_OF_WORKING_DAYS = 20;
             int empHrs = 0;
             int empWage = 0;
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            if (empCheck == IS_PART_TIME )
+
+            int totalEmpWage = 0;
+            int totalEmpHours = 0;
+
+
+            for (int i = 0; i < NUM_OF_WORKING_DAYS; i++)
             {
-                empHrs = 4;
+
+
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                if (empCheck == IS_PART_TIME)
+                {
+                    empHrs = 4;
+                }
+                else if (empCheck == IS_FULL_TIME)
+                {
+                    empHrs = 8;
+                }
+                totalEmpHours=totalEmpHours+empHrs;
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                Console.WriteLine("empwage  : " + empWage);
             }
-            else if(empCheck == IS_FULL_TIME )
-            {
-                empHrs = 8;
-            }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine(  "empwage  : "+empWage );
+            totalEmpWage=totalEmpHours*EMP_RATE_PER_HOUR;
+            Console.WriteLine("Total EmpWage is : "+totalEmpWage);
+
         }
     }
 }
