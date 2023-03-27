e2bc907a37812d6a9207488ecfe74391bde10dd9
shrutik0501 <shrutik0501@gmail.com>
2023-03-27 12:34:20 +0530

    my UC4

--- EmployeeAttendance.cs
+++ EmployeeAttendance.cs
@@ -6,42 +6,31 @@ using System.Threading.Tasks;
 
 namespace EmployeeManagement
 {
-    internal class EmployeeAttendance
+    class EmployeeAttendance
     {
-        public static void GetAttendence()
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+       public static void EmpWageUsingSwitchCase()
         {
-            int IS_PART_TIME  = 1;
-            int IS_FULL_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
-            int NUM_OF_WORKING_DAYS = 20;
             int empHrs = 0;
             int empWage = 0;
-
-            int totalEmpWage = 0;
-            int totalEmpHours = 0;
-
-
-            for (int i = 0; i < NUM_OF_WORKING_DAYS; i++)
+            Random random = new Random();
+            int empcheck = random.Next(0, 3);
+            switch (empcheck)
             {
-
-
-                Random random = new Random();
-                int empCheck = random.Next(0, 3);
-                if (empCheck == IS_PART_TIME)
-                {
+                case IS_PART_TIME:
                     empHrs = 4;
-                }
-                else if (empCheck == IS_FULL_TIME)
-                {
+                    break;
+                case IS_FULL_TIME:
                     empHrs = 8;
-                }
-                totalEmpHours=totalEmpHours+empHrs;
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                Console.WriteLine("empwage  : " + empWage);
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
-            totalEmpWage=totalEmpHours*EMP_RATE_PER_HOUR;
-            Console.WriteLine("Total EmpWage is : "+totalEmpWage);
-
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage : " + empWage);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Hello, World!");
 
-            EmployeeAttendance.GetAttendence();
+            EmployeeAttendance.EmpWageUsingSwitchCase();
 
         }
 }
