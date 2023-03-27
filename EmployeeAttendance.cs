063ed8eb335db5a2806da9dbd1a313c473642736
shrutik0501 <shrutik0501@gmail.com>
2023-03-27 13:00:06 +0530

    this is my UC5

--- EmployeeAttendance.cs
+++ EmployeeAttendance.cs
@@ -11,26 +11,33 @@ namespace EmployeeManagement
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
-       public static void EmpWageUsingSwitchCase()
+        public const int NUM_OF_WORKING_DAYS = 20;
+        public static void EmpWageUsingForLoop()
         {
             int empHrs = 0;
             int empWage = 0;
-            Random random = new Random();
-            int empcheck = random.Next(0, 3);
-            switch (empcheck)
+            int totalEmpWage = 0;
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
             {
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                Random random = new Random();
+                int empcheck = random.Next(0, 3);
+                switch (empcheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("Emp Wage : " + empWage);
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage : " + empWage);
+            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Hello, World!");
 
-            EmployeeAttendance.EmpWageUsingSwitchCase();
+            EmployeeAttendance.EmpWageUsingForLoop();
 
         }
 }
